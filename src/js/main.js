76812bd5b3a1abe8d3b418af266e8b2d485902fe
roman <gordonfreeman2332@gmail.com>
2019-02-27 01:15:06 +0300

    финальный коммит валидация исправлена

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,18 @@
-const form = document.querySelector('#myForm');
-const inputs = form.querySelectorAll('.card__frontside-number__block-value');
-const owner = form.querySelector('.card__frontside-owner-value');
+const myForm = document.querySelector('#myForm');
+const inputs = myForm.querySelectorAll('.card__frontside-number__block-value');
+const owner = myForm.querySelector('.card__frontside-owner-value');
+const subBtn = myForm.querySelector('#submit');
 
+subBtn.addEventListener('click', e => {
+    event.preventDefault();
+    if(validateForm(myForm)) {
+        myForm.reset();
+    }
+});
 for (const input of inputs) {
-    input.addEventListener('keydown', function(event) {
+   input.addEventListener('keydown', function(event) {
         validation(input);
-    });
+  });
 }
 
 $('.card__backside__cvv-text-value').keydown(function(event){
@@ -45,3 +52,37 @@ function validation() {
         console.log(event.key);
     }
 };
+
+
+
+function validateForm(form) {
+    let valid = true;
+
+
+    for (const input of inputs) {
+        if (!validateField(input)) {
+            valid = false;
+        }
+    }
+   
+
+    if (!validateField(owner)) {
+        valid = false;
+    }
+
+    if (!validateField(form.elements.cvv)) {
+        valid = false;
+    }
+    return valid;
+} 
+
+function validateField(field) {
+    if (!field.checkValidity()){
+        field.classList.add('error');
+        return false;
+    }
+    else {
+        field.classList.remove('error');
+        return true;
+    }
+}
