cb27833e68954bb36ea7aaf7c735ccdf717425d2
roman <gordonfreeman2332@gmail.com>
2019-02-27 01:45:04 +0300

    весь процесс был откоментирован внутри рабочих исходников

--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,22 @@ const myForm = document.querySelector('#myForm');
 const inputs = myForm.querySelectorAll('.card__frontside-number__block-value');
 const owner = myForm.querySelector('.card__frontside-owner-value');
 const subBtn = myForm.querySelector('#submit');
-
+//переменные необходимые для валидации формы
 subBtn.addEventListener('click', e => {
     event.preventDefault();
     if(validateForm(myForm)) {
         myForm.reset();
     }
-});
+});//обработчик событий и предотвратитель действия по клику на сабмит формы
 for (const input of inputs) {
    input.addEventListener('keydown', function(event) {
         validation(input);
-  });
+  });//проверка инпутов на необходимые символы
 }
 
 $('.card__backside__cvv-text-value').keydown(function(event){
     validation(owner);
-});
+});//валидация формы владельца выполнена с использованием jQuery
 $(owner).keydown(e =>{
     let isLat = false;
     let isControl = false;
@@ -35,7 +35,7 @@ $(owner).keydown(e =>{
         event.preventDefault();
         console.log(event.key);
     }
-});
+});//функция валидации данных
 function validation() {
     let isDigit = false;
     let isControl = false;
@@ -54,28 +54,28 @@ function validation() {
 };
 
 
-
+//функция валидации формы
 function validateForm(form) {
     let valid = true;
 
-
+    //проверка каждого инпута
     for (const input of inputs) {
         if (!validateField(input)) {
             valid = false;
         }
     }
    
-
+    //проверка владельца карты
     if (!validateField(owner)) {
         valid = false;
     }
-
+    //проверка валидации свв кода
     if (!validateField(form.elements.cvv)) {
         valid = false;
     }
     return valid;
 } 
-
+//функция валидации полей присваивание стилей в зависимости от данных
 function validateField(field) {
     if (!field.checkValidity()){
         field.classList.add('error');
